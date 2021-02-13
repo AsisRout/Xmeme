0a3f7e5a6e41f9f94768defa52c0b55350d59d0a
Asis Rout <asisrout@localhost.localdomain>
2021-02-14 02:10:52 +0530

    Update

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ app.use(cors())
 
 
 // connect to mongodb 
-//const dbURI = 'mongodb+srv://asis123:265rohit@cluster0.xbfv4.mongodb.net/xmeme?retryWrites=true&w=majority';
+// const dbURI = 'mongodb+srv://asis123:265rohit@cluster0.xbfv4.mongodb.net/xmeme?retryWrites=true&w=majority';
 const dbURI =  "mongodb://mongo:27017/meme";
 
 
